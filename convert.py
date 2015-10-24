0ac373b0c17185823bac562ad6a99703fa70d134
Den Raskovalov <denbox@gmail.com>
2015-10-24 00:18:53 -0700

    read dict

--- convert.py
+++ convert.py
@@ -2,6 +2,7 @@
 
 import pickle
 import argparse
+import random
 
 parser = argparse.ArgumentParser(description="prepare data for serps modeling")
 parser.add_argument('-l', "--limit", type=int, help="limir", default=max)
@@ -10,8 +11,18 @@ args = parser.parse_args()
 
 limit = args.limit
 
-sentences = []
-labels = []
+dictionary = {}
+dictionary[0] = "<UNK>"
+index = 1
+for line in open("2015-10-01-False-TopHosts2"):
+    parts = line.split("\t")
+    dictionary[index] = parts[0]
+    index += 1
+
+train_x = []
+train_y = []
+test_x = []
+test_y = []
 
 iLine = 0
 for line in open("2015-10-01-False-SAT"):
@@ -23,7 +34,7 @@ for line in open("2015-10-01-False-SAT"):
 
     parts = line.split("\t")
     
-    labels.append( int(parts[1]) )
+    label = int(parts[1])
 
     sentence = []
     sites = parts[2].split("*")
@@ -33,11 +44,20 @@ for line in open("2015-10-01-False-SAT"):
             sentence.append(int(siteParts[0]))
     sentences.append(sentence)
 
+    if random.randint(0, 10) < 8:
+        train_x.append(sentence)
+        train_y.append(label)
+    else:
+        test_x.append(sentence)
+        test_y.append(label)
+
     iLine += 1
-
-data = {}
-data["sentences"] = sentences
-data["labels"] = labels
-
+    
 with open("data.pkl", "wb") as f:
-    pickle.dump(data, f)
+    pkl.dump((train_x, train_y), f, -1)
+    pkl.dump((test_x, test_y), f, -1)
+
+    revDict = {}
+    for k, v in dictionary.items():
+        revDict[v] = k
+    pkl.dump(revDict, f, -1)
