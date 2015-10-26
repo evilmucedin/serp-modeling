ae99997878d865f60d6b3409a81797dfc107b5ae
Den Raskovalov <denplusplus@yandex-team.ru>
2015-10-26 09:24:29 -0700

    faster stat

--- convert.py
+++ convert.py
@@ -5,19 +5,11 @@ import argparse
 import random
 
 parser = argparse.ArgumentParser(description="prepare data for serps modeling")
-parser.add_argument('-l', "--limit", type=int, help="limir", default=max)
+parser.add_argument('-l', "--limit", type=int, help="limir", default=2**32)
 
 args = parser.parse_args()
 
 limit = args.limit
-
-dictionary = {}
-dictionary[0] = "<UNK>"
-index = 1
-for line in open("2015-10-01-False-TopHosts2"):
-    parts = line.split("\t")
-    dictionary[index] = parts[0]
-    index += 1
 
 train_x = []
 train_y = []
@@ -25,9 +17,10 @@ test_x = []
 test_y = []
 
 iLine = 0
+sumLabels = 0.0
 for line in open("2015-10-01-False-SAT"):
-    if 0 == (iLine % 100000):
-        print("... %d" % iLine)
+    if 0 == (iLine % 100000) and (iLine != 0):
+        print("... %d %f" % (iLine, sumLabels/iLine))
 
     if iLine > limit:
         break
@@ -35,6 +28,7 @@ for line in open("2015-10-01-False-SAT"):
     parts = line.split("\t")
     
     label = int(parts[1])
+    sumLabels += label
 
     sentence = []
     sites = parts[2].split("*")
@@ -42,22 +36,26 @@ for line in open("2015-10-01-False-SAT"):
         siteParts = site.split("|")
         if 3 == len(siteParts):
             sentence.append(int(siteParts[0]))
-    sentences.append(sentence)
 
-    if random.randint(0, 10) < 8:
-        train_x.append(sentence)
-        train_y.append(label)
-    else:
-        test_x.append(sentence)
-        test_y.append(label)
+    if 0 != len(sentence):
+        if random.randint(0, 10) < 8:
+            train_x.append(sentence)
+            train_y.append(label)
+        else:
+            test_x.append(sentence)
+            test_y.append(label)
 
     iLine += 1
     
-with open("data.pkl", "wb") as f:
-    pkl.dump((train_x, train_y), f, -1)
-    pkl.dump((test_x, test_y), f, -1)
+revDict = {}
+revDict[0] = "<UNK>"
+index = 1
+for line in open("2015-10-01-False-TopHosts2"):
+    parts = line.split("\t")
+    revDict[index] = parts[0]
+    index += 1
 
-    revDict = {}
-    for k, v in dictionary.items():
-        revDict[v] = k
-    pkl.dump(revDict, f, -1)
+with open("data.pkl", "wb") as f:
+    pickle.dump((train_x, train_y), f, -1)
+    pickle.dump((test_x, test_y), f, -1)
+    pickle.dump(revDict, f, -1)
